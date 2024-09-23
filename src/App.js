b5094bb7c42c379375aab02107685df750f6337f
eduardo <edmunoz1030@gmail.com>
2024-09-22 23:18:06 -0700

    added scrollsmooth for testing

--- src/App.js
+++ src/App.js
@@ -21,22 +21,33 @@
 
 // export default App;
 
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 import React, { useEffect } from 'react';
 import { gsap } from 'gsap';
 import './App.css';
 
+gsap.registerPlugin(ScrollSmoother);
+
 function App() {
   useEffect(() => {
+    ScrollSmoother.create({
+      wrapper: "#smooth-wrapper",
+      content: "#smooth-content",
+      smooth: 1, // duration to "catch up"
+      effects: true
+    })
     // Basic GSAP animation: Fade in the header on load
     gsap.from('.App-header', { opacity: 0, duration: 2, y: -50 });
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div id="smooth-wrapper" className="App">
+      <div id="smooth-content">
+        <header className="App-header">
         <h1>HELLO!</h1>
         <p>This site is animated with GSAP!</p>
       </header>
+      </div>
     </div>
   );
 }
