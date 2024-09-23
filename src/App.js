8d2af5958c43fc63b2b107ea324b2b3e5ee8b331
eduardo <edmunoz1030@gmail.com>
2024-09-22 19:03:17 -0700

    added gsap app test run

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import React from 'react';
-import Home from './Home';
-import About from './About';
-import Projects from './Projects';
-import Contact from './Contact';
-import './App.css';  // Add styles in this file
-import Tabs from './Tabs';
+// import React from 'react';
+// import Home from './Home';
+// import About from './About';
+// import Projects from './Projects';
+// import Contact from './Contact';
+// import './App.css';  // Add styles in this file
+// import Tabs from './Tabs';
+
+// function App() {
+//     return (
+//         <div className="App">
+//           <Tabs>
+//             <Home />
+//             <About />
+//             <Projects />
+//             <Contact />
+//           </Tabs>
+//         </div>
+//     );
+// }
+
+// export default App;
+
+import React, { useEffect } from 'react';
+import { gsap } from 'gsap';
+import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-          <Tabs>
-            <Home />
-            <About />
-            <Projects />
-            <Contact />
-          </Tabs>
-        </div>
-    );
+  useEffect(() => {
+    // Basic GSAP animation: Fade in the header on load
+    gsap.from('.App-header', { opacity: 0, duration: 2, y: -50 });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Cool Website</h1>
+        <p>This site is animated with GSAP!</p>
+      </header>
+    </div>
+  );
 }
 
 export default App;
