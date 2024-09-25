9419ac07b445e5acf2721da6207d0b83dc628f4b
eduardo <edmunoz1030@gmail.com>
2024-09-25 15:22:09 -0700

    few test changes

--- src/App.js
+++ src/App.js
@@ -21,22 +21,49 @@
 
 // export default App;
 
+// import React, { useEffect } from 'react';
+// import { gsap } from 'gsap';
+// import './App.css';
+// import About from './About';
+// import Tabs from './Tabs';
+
+// function App() {
+//   useEffect(() => {
+//     // Basic GSAP animation: Fade in the header on load
+//     gsap.from('.App-header', { opacity: 0, duration: 2, y: -50 });
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <Tabs>
+//         <About />
+//       </Tabs>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, { useEffect } from 'react';
 import { gsap } from 'gsap';
 import './App.css';
+import Home from './Home';
 import About from './About';
+import Projects from './Projects';
 import Tabs from './Tabs';
 
 function App() {
   useEffect(() => {
-    // Basic GSAP animation: Fade in the header on load
     gsap.from('.App-header', { opacity: 0, duration: 2, y: -50 });
   }, []);
 
   return (
     <div className="App">
+      <p>Hello!</p>
       <Tabs>
-        <About />
+        <Home label="Home" />
+        <About label="About" />
+        <Projects label="Projects" />
       </Tabs>
     </div>
   );
