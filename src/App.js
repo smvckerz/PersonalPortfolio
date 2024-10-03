e7b8954bfbf421b949f405a8d09804e0d68507c4
eduardo <edmunoz1030@gmail.com>
2024-10-03 00:07:37 -0700

    Tets

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello!</p>
+        <p>Hell!</p>
       </header>
       <Tabs>
         <Home label="Home" />
