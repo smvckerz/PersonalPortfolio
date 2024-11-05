e3c7e9a274cf9d053af353d3bf79979eadbfd73a
smvckerz <104466989+smvckerz@users.noreply.github.com>
2024-11-04 23:14:03 -0800

    Hello

--- src/App.js
+++ src/App.js
@@ -1,49 +1,3 @@
-// import React from 'react';
-// import Home from './Home';
-// import About from './About';
-// import Projects from './Projects';
-// import Contact from './Contact';
-// import './App.css';  // Add styles in this file
-// import Tabs from './Tabs';
-
-// function App() {
-//     return (
-//         <div className="App">
-//           <Tabs>
-//             <Home />
-//             <About />
-//             <Projects />
-//             <Contact />
-//           </Tabs>
-//         </div>
-//     );
-// }
-
-// export default App;
-
-// import React, { useEffect } from 'react';
-// import { gsap } from 'gsap';
-// import './App.css';
-// import About from './About';
-// import Tabs from './Tabs';
-
-// function App() {
-//   useEffect(() => {
-//     // Basic GSAP animation: Fade in the header on load
-//     gsap.from('.App-header', { opacity: 0, duration: 2, y: -50 });
-//   }, []);
-
-//   return (
-//     <div className="App">
-//       <Tabs>
-//         <About />
-//       </Tabs>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { useEffect } from 'react';
 import { gsap } from 'gsap';
 import './App.css';
@@ -60,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hell!</p>
+        <p>Hello!</p>
       </header>
       <Tabs>
         <Home label="Home" />
