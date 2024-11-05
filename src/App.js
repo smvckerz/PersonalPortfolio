48b50a9c9dfbe29fa84ac2ba071fa920f541c9d4
smvckerz <104466989+smvckerz@users.noreply.github.com>
2024-11-04 23:19:46 -0800

    testing a few chages in home.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import React, { useEffect } from 'react';
-import { gsap } from 'gsap';
 import './App.css';
 import Home from './Home';
 import About from './About';
@@ -7,17 +5,8 @@ import Projects from './Projects';
 import Tabs from './Tabs';
 
 function App() {
-  // useEffect(() => {
-  //   gsap.from('.App-header', { opacity: 0, duration: 2, y: -50 });
-  // }, []);
-
   return (
-    // <div className="App">
      <div>
-      {/* <header className="App-header"> */}
-      <header>
-        <p>Hello!</p>
-      </header>
       <Tabs className="App-header">
         <Home label="Home" />
         <About label="About" />
--- src/Home.js
+++ src/Home.js
@@ -3,8 +3,10 @@ import React from 'react';
 function Home() {
     return (
         <section id="home" className="section-home">
+            <center>
             <h1>Welcome to My Portfolio</h1>
             <p>I'm Eduardo Munoz, a Software Engineer.</p>
+            </center>
         </section>
     );
 }
