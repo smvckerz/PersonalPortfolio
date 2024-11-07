f38e06d7bb5d3a0835e048daf5a96cbd47834f92
eduardo <edmunoz1030@gmail.com>
2024-11-07 15:27:17 -0800

    Testing loading screen

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import Home from './Home';
 import About from './About';
 import Projects from './Projects';
 import Tabs from './Tabs';
+import './Home'
+import PreLoader1 from './PreLoader1';
 
 function App() {
   return (
      <div>
+      <PreLoader1/>
       <Tabs className="App-header">
         <Home label="Home" />
         <About label="About" />
