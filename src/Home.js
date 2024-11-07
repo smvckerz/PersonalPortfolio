df5059d798f844214705a7e3f9bf0470b0615fc2
smvckerz <104466989+smvckerz@users.noreply.github.com>
2024-11-06 22:21:04 -0800

    testing new

--- src/Home.js
+++ src/Home.js
@@ -60,31 +60,90 @@
 
 // export default Home;
 
-import React, {useState} from 'react'
-import { param } from "framer-motion/client";
-import testUnlimScroll from './testUnlimScroll';
+// import React, {useState} from 'react'
+// import useTestUnlimScroll from './testUnlimScroll';
 
-function handleSearch(e){
-    setQuery(e.target.value)
-    setPageNumber(1)
-}
+// function handleSearch(e){
+//     setQuery(e.target.value)
+//     setPageNumber(1)
+// }
+
+// const {
+//     books,
+//     hasMore,
+//     loading,
+//     error
+// } = useBookSearch(query, pageNumber)
 
 
-function Home() {
-    const [query, setQuery] = useState('')
-    const [pageNumber, setPageNumber] = useState('')
-    testUnlimScroll(query, pageNumber)
-    return (
-        <><input type='text' onChange={handleSearch}>
+// function Home() {
+//     const [query, setQuery] = useState('')
+//     const [pageNumber, setPageNumber] = useState(1)
+//     useTestUnlimScroll(query, pageNumber)
+//     return (
+//         <><input type='text' onChange={handleSearch}>
 
-        </input>
-        <section id="home" className="section-home">
-            <center>
-                <h1>Welcome to My Portfolio</h1>
-                <p>I'm Eduardo Munoz, a Software Engineer.</p>
-            </center>
-        </section></>
-    );
-}
+//         </input>
+//         {books.map(book => {
+//             return <div key={book}>{book}</div>
+//         })}
+//         <section id="home" className="section-home">
+//             <center>
+//                 <h1>Welcome to My Portfolio</h1>
+//                 <p>I'm Eduardo Munoz, a Software Engineer.</p>
+//             </center>
+//         </section></>
+//     );
+// }
 
-export default Home;
+// export default Home;
+
+import React, { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import axios from "axios";
+import ProductCard from "./ProductCard";
+import Loader from "./Loader";
+
+const InfiniteScrollExample1 = () => {
+  const [items, setItems] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+  const [index, setIndex] = useState(2);
+
+  useEffect(() => {
+    axios
+      .get("https://api.escuelajs.co/api/v1/products?offset=10&limit=12")
+      .then((res) => setItems(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const fetchMoreData = () => {
+    axios
+      .get(`https://api.escuelajs.co/api/v1/products?offset=${index}0&limit=12`)
+      .then((res) => {
+        setItems((prevItems) => [...prevItems, ...res.data]);
+
+        res.data.length > 0 ? setHasMore(true) : setHasMore(false);
+      })
+      .catch((err) => console.log(err));
+
+    setIndex((prevIndex) => prevIndex + 1);
+  };
+
+  return (
+    <InfiniteScroll
+      dataLength={items.length}
+      next={fetchMoreData}
+      hasMore={hasMore}
+      loader={<Loader />}
+    >
+      <div className='container'>
+        <div className='row'>
+          {items &&
+            items.map((item) => <ProductCard data={item} key={item.id} />)}
+        </div>
+      </div>
+    </InfiniteScroll>
+  );
+};
+
+export default InfiniteScrollExample1;
