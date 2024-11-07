974bd5238b4f2cc9afa4d0cf65960c04b3b8087a
eduardo <edmunoz1030@gmail.com>
2024-11-07 15:31:52 -0800

    Testing loading screen

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
+// App.js
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Home from './Home';
 import About from './About';
 import Projects from './Projects';
 import Tabs from './Tabs';
-import './Home'
 import PreLoader1 from './PreLoader1';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2000); // Example: 2 seconds
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-     <div>
-      <PreLoader1/>
-      <Tabs className="App-header">
-        <Home label="Home" />
-        <About label="About" />
-        <Projects label="Projects" />
-      </Tabs>
+    <div>
+      {loading ? (
+        <PreLoader1 />
+      ) : (
+        <Tabs className="App-header">
+          <Home label="Home" />
+          <About label="About" />
+          <Projects label="Projects" />
+        </Tabs>
+      )}
     </div>
   );
 }
