fcd14d345f9c9dd34875a0a43ff8b4abb92df572
eduardo <edmunoz1030@gmail.com>
2024-12-23 13:24:46 -0800

    Changed css for index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; // optional CSS file
+// import './index.css'; // optional CSS file
 import App from './App';
 
 ReactDOM.render(
