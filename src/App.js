8201b1039d7456a330c9d26c0916c6c1a5f0d250
eduardo <edmunoz1030@gmail.com>
2024-12-31 17:12:46 -0800

    Testing loader screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 // App.js
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Home from "./Home";
+import Loading from "./Loading";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a 2-second loading time
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    // Cleanup if component unmounts before 2s
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      <Home />
+      {loading ? <Loading /> : <Home />}
     </div>
   );
 }
---
+++ src/Loading.js
@@ -0,0 +1,14 @@
+// Loading.js
+import React from "react";
+import "./Loading.css";
+
+function Loading() {
+  return (
+    <div className="loading-container">
+      <div className="spinner"></div>
+      <p>Loading...</p>
+    </div>
+  );
+}
+
+export default Loading;
