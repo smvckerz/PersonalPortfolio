6dbb6e86e05f97a48811e0910a7ec79106367d9d
Eduardo Munoz <edmunoz1030@gmail.com>
2025-10-02 22:24:24 -0700

    final testing of CI hopefully

--- src/Website.js
+++ src/Website.js
@@ -52,7 +52,7 @@ export default function Website() {
           <a href="#projects">Projects</a>
           <a href="#skills">Skills</a>
           <a href="#contact">Contact</a>
-          <a href="#tester"a>
+          <a href="#tester"a></a>
          <Link to="/console" className="btn small" style={{ marginLeft: 12 }}>
             Open Console
          </Link>
